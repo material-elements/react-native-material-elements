d52ccb879d1344f08f002f60f4524fab711ab397
dheeraj1429 <dheerajsingh1429@gmail.com>
2025-03-06 21:59:56 +0530

    Added theme function docs

--- src/components/TextField/BaseInput.tsx
+++ src/components/TextField/BaseInput.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { StyleSheet, TextInput } from 'react-native';
 import { useThemeColorsSelector } from '../../libraries';
 import { BaseInputProps } from './Input.types';
@@ -7,9 +7,11 @@ import { baseInputStyles } from './TextField.style';
 export const BaseInput = React.forwardRef<TextInput, BaseInputProps>(({ style, variant, height, ...props }, ref) => {
   const themeColors = useThemeColorsSelector();
 
-  const baseInputGeneratedStyles = useMemo(
-    () => baseInputStyles({ colors: themeColors, variant, height }),
-    [variant, themeColors, height],
+  return (
+    <TextInput
+      ref={ref}
+      style={StyleSheet.flatten([baseInputStyles({ colors: themeColors, variant, height }), style])}
+      {...props}
+    />
   );
-  return <TextInput ref={ref} style={StyleSheet.flatten([baseInputGeneratedStyles, style])} {...props} />;
 });
--- src/libraries/themes/v1/V2ThemeContext.tsx
+++ src/libraries/themes/v1/V2ThemeContext.tsx
@@ -5,7 +5,7 @@ import { createContext, useContext } from 'use-context-selector';
 import { initialDarkTheme, initialLightTheme } from './colors';
 import { font, fontWeight, latterSpacing, lineHeight, spacing } from './sizes';
 import {
-  ColorShades,
+  CreateColorShades,
   CreateColorShadesInterface,
   CreateThemeDimensions,
   CreateThemeDimensionsReturnValues,
@@ -15,6 +15,7 @@ import {
   ThemeKeys,
   ThemeProviderProps,
   ThemeType,
+  UseTheme,
 } from './theme';
 
 export const themeDimensions: ThemeDimensions = {
@@ -38,11 +39,18 @@ export const ThemeContext = createContext<ThemeInterface<ThemeType> | undefined>
 /**
  * Function to create color shades by merging default theme colors with custom shades
  * @returns ColorShades
+ *
+ * @example
+ * ```tsx
+ * const colors = createColorShades({
+ *    shades: { 400: '#000000' },
+ *    themePropertyName: 'green'
+ * })
  */
 export const createColorShades = <T extends string>({
   shades,
   themePropertyName,
-}: CreateColorShadesInterface<T>): ColorShades => ({
+}: CreateColorShadesInterface<T>): CreateColorShades => ({
   ...defaultLightTheme.colors[themePropertyName as ThemeKeys],
   ...shades,
 });
@@ -50,6 +58,14 @@ export const createColorShades = <T extends string>({
 /**
  * Function to create them dimensions by merging default dimensions with custom dimensions.
  * @returns CreateThemeDimensionsReturnValues
+ *
+ * @example
+ * ```tsx
+ * const themeDimensions = createThemeDimensions({
+ *    spacing: {
+ *      xs: 10
+ *  },
+ * });
  */
 export const createThemeDimensions = (dimensions: CreateThemeDimensions): CreateThemeDimensionsReturnValues =>
   _.merge({}, themeDimensions, dimensions);
@@ -60,13 +76,70 @@ export const createThemeDimensions = (dimensions: CreateThemeDimensions): Create
  * @param mode ColorSchemeName
  * @param theme CreateThemeType
  * @returns CreateThemeReturnValues
+ *
+ * @example
+ * ```tsx
+ * const lightTheme = createTheme('light', {
+ * colors: {
+ *    green: createColorShades({
+ *      shades: { 400: '#000000' },
+ *      themePropertyName: 'green'
+ *    })
+ *  }
+ * });
  */
 export const createTheme = function <T>(mode: ColorSchemeName, theme: CreateThemeType & T) {
   const isLightTheme = mode === 'light';
   return _.merge({}, isLightTheme ? defaultLightTheme : defaultDarkTheme, theme);
 };
 
-export const ThemeProvider = ({ children, lightTheme, darkTheme, dimensions, components = {} }: ThemeProviderProps) => {
+/**
+ * The ThemeProvider is used to provide theming capabilities to your application, allowing you to switch between light and dark
+ * themes and customize various style properties.
+ *
+ * @param {React.ReactNode} props.children - The child components that will receive theme context.
+ * @param {Theme} props.lightTheme - The theme object used in light mode.
+ * @param {Theme} props.darkTheme - The theme object used in dark mode.
+ * @param {Dimensions} [props.dimensions] - Optional dimensions for theme calculations.
+ * @param {Record<string, any>} [props.components={}] - Custom component styles merged with the theme.
+ *
+ * @returns {JSX.Element} A ThemeContext provider that wraps the application.
+ *
+ * @example
+ * ```tsx
+ * const lightTheme = createTheme('light', {
+ * colors: {
+ *    green: createColorShades({
+ *      shades: { 400: '#000000' },
+ *      themePropertyName: 'green'
+ *    })
+ *  }
+ * });
+ *
+ * const darkTheme = createTheme('light', {
+ * colors: {
+ *    green: createColorShades({
+ *      shades: { 400: '#d54d4d' },
+ *      themePropertyName: 'green',
+ *    }),
+ *  }
+ * });
+ *
+ * const themeDimensions = createThemeDimensions({
+ *    spacing: {
+ *      xs: 10
+ *  },
+ * });
+ *
+ * <ThemeProvider lightTheme={lightTheme} darkTheme={darkTheme} dimensions={themeDimensions}>{children}</ThemeProvider>
+ */
+export const ThemeProvider = ({
+  children,
+  lightTheme,
+  darkTheme,
+  dimensions,
+  components = {},
+}: ThemeProviderProps): JSX.Element => {
   const colorScheme = useColorScheme();
 
   const initialTheme = useMemo(() => {
@@ -82,7 +155,7 @@ export const ThemeProvider = ({ children, lightTheme, darkTheme, dimensions, com
   return <ThemeContext.Provider value={themeValues}>{children}</ThemeContext.Provider>;
 };
 
-export const useTheme = () => {
+export const useTheme = (): UseTheme => {
   const context = useContext(ThemeContext);
   if (!context) {
     throw new Error('Theme context must be used within a ThemeProvider');
