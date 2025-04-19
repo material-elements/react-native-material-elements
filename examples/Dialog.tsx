1ace200ea015ca66deff70b675d0fcfd69775c9c
dheeraj1429 <dheerajsingh1429@gmail.com>
2025-04-19 13:41:37 +0530

    Added dialog example

--- examples/Dialog.tsx
+++ examples/Dialog.tsx
@@ -1,24 +1,19 @@
-import React, { useState } from 'react';
-import { Button, ButtonGroup, Dialog, DialogContent, DialogContentText, DialogTitle } from '../src';
+import React from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider } from '../src';
 
 export const Ex1: React.FC = () => {
-  const [showDialog, setShowDialog] = useState(false);
-
   return (
-    <>
-      <Dialog maxWidth="xl" visible={showDialog} onClose={() => setShowDialog(false)}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText gutterBottom>
-            To subscribe to this website, please enter your email address here. We will send updates occasionally.
-          </DialogContentText>
-        </DialogContent>
-        <ButtonGroup style={{ paddingHorizontal: 10 }} variation="text">
-          <Button label="Done" onPress={() => {}} flex={1} />
-          <Button label="Close" onPress={() => {}} flex={1} />
-        </ButtonGroup>
-      </Dialog>
-      <Button onPress={() => setShowDialog(!showDialog)} label="Open" />
-    </>
+    <Dialog visible>
+      <DialogTitle>Subscribe</DialogTitle>
+      <Divider />
+      <DialogContent>
+        <DialogContentText gutterBottom>
+          To subscribe to this website, please enter your email address here. We will send updates occasionally.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button size="medium" variation="text" label="Save" square onPress={() => {}} />
+      </DialogActions>
+    </Dialog>
   );
 };
