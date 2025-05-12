54e5c6bc01a0d9e0e26342fec9d77a6bd499cfe8
dheeraj1429 <dheerajsingh1429@gmail.com>
2025-05-12 16:16:05 +0530

    Fixed the working issue in useThemedProps hook

--- src/hooks/useThemedProps.ts
+++ src/hooks/useThemedProps.ts
@@ -1,6 +1,7 @@
 import React, { isValidElement, useMemo } from 'react';
 import { useThemeColorsSelector } from '../libraries';
 import { Theme } from '../libraries/themes/types';
+import _ from 'lodash';
 
 export type ThemedIconProp = ((theme: Theme) => React.ReactNode) | React.ReactNode;
 type ThemedProp<T = any> = T | ((themeColors: ReturnType<typeof useThemeColorsSelector>) => T);
@@ -13,15 +14,20 @@ export const useThemedProps = <T extends Record<string, ThemedProp>>(props: T) =
       [K in keyof T]: React.ReactNode;
     };
 
-    for (const key in props) {
-      const value = props[key];
-      if (value && typeof value === 'function') {
-        resolvedProps[key] = value(themeColors);
-      } else if (isValidElement(value)) {
-        resolvedProps[key] = value;
-      } else {
-        console.warn('icon prop must be either <Icon /> or () => <Icon />. Other values are not valid.');
-        continue;
+    if (!_.isEmpty(props)) {
+      for (const key in props) {
+        const value = props[key];
+
+        if (value == null || value == undefined) continue;
+
+        if (value && typeof value === 'function') {
+          resolvedProps[key] = value(themeColors);
+        } else if (isValidElement(value)) {
+          resolvedProps[key] = value;
+        } else {
+          console.warn('icon prop must be either <Icon /> or () => <Icon />. Other values are not valid.');
+          continue;
+        }
       }
     }
 
