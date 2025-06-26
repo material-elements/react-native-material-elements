ea9fab5c2c3851e70240adef6c46144d4ab515ed
dheeraj1429 <dheerajsingh1429@gmail.com>
2025-06-26 12:28:11 +0530

    Updated jest-config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  collectCoverageFrom: ['src/**/*.{js,ts,tsx}'],
+
+  coveragePathIgnorePatterns: [
+    '/node_modules',
+    './examples',
+    './docs',
+    './__test__',
+    'src/libraries/themes/v1/colors',
+    'src/libraries/themes/v1/sizes',
+    '.styles.ts',
+    'index.ts',
+    'd.ts',
+    '.consts.ts',
+    '.enum.ts',
+    '/config/',
+    '/coverage/',
+  ],
+
+  coverageReporters: ['html', 'text', 'lcov', 'text-summary'],
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'mjs', 'svg', 'png'],
+  testEnvironment: 'node',
+
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
