915ffd1ead0fa4828860714236d9e262c312a9e0
Dee <86516056+dheeraj1429@users.noreply.github.com>
2025-09-21 00:34:06 +0530

    [workflow/test] Added eslint config =

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
   extends: ['@react-native', 'plugin:testing-library/react'],
+  ignorePatterns: ['*.js', '*.ts', '*.tsx', 'jest.*', 'metro.config.js', '.eslintrc.js'],
+  overrides: [
+    {
+      files: ['src/**/*.{js,jsx,ts,tsx}'],
+      rules: {
+        'object-curly-spacing': ['error', 'always'],
+        'unused-imports/no-unused-imports': 'error',
+      },
+    },
+  ],
+  env: { jest: true },
+  parserOptions: {
+    sourceType: 'module',
+    useJSXTextNode: true,
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  plugins: ['unused-imports'],
 };
